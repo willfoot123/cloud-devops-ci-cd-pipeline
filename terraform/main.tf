3af9f2f9d9072403d76748d4a663956b19d6951c
willfoot123 <willhescore@gmail.com>
2026-06-15 13:13:46 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "devops_server" {
+  ami           = "ami-0c76bd4bd302b30ec"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DevOpsInstance"
+  }
+}
 
